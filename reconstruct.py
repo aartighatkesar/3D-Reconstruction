3d7d839d089bc16f08c155d215da06d7b532555b
Aarti Ghatkesar <aarti@atollogy.com>
2020-03-09 08:17:33 -0700

    Dataset and initial script

---
+++ reconstruct.py
@@ -0,0 +1,44 @@
+
+
+class ProjectiveReconstruction:
+    def __init__(self):
+        pass
+
+    def _build_F_homogenous_eqns(self):
+        pass
+
+    def calculate_F_linear_LS(self):
+        pass
+
+    def triangulate(self):
+        pass
+
+    def compute_SIFT_features(self):
+        pass
+
+    def get_corresponding_pts(self):
+        pass
+
+
+    def rectify_image(self):
+        pass
+
+
+    def compute_canny_edges(self):
+        pass
+
+
+    def cost_F_non_linear_LS(self):
+        pass
+
+
+    def optimize_F_non_linear_LS(self):
+        pass
+
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
